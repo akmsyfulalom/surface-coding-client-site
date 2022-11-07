ea314dd9d02fb170b2d3ff365d508d2b6ffbaa6a
akmsyfulalom <akmsyfulbd12@gmail.com>
2022-11-08 05:24:11 +0600

    I finished everything completely

--- src/pages/Faq/Faq.js
+++ src/pages/Faq/Faq.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaChevronCircleDown, FaChevronCircleUp } from 'react-icons/fa';
 
 const Faq = () => {
+
     return (
         <div>
-            <h2>this is FAQ components</h2>
+            <p className='my-24 font-bold text-2xl text-gray-800'>
+                We are bringing your frequently asked questions very soon
+                <p>Thank you so much </p>
+            </p>
+
         </div>
     );
 };
